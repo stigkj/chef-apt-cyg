07934517a117f24af5bafc8f5a627c6a99813b17
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-20 16:12:06 +0200

    First cut on a list of default packages to have installed on developer's workstations

--- nos/recipes/default.rb
+++ nos/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+cyg_package "mintty" do
+  action :install
+end
+
+cyg_package "zsh" do
+  action :install
+end
+
+gem_package "git-up" do
+  action :install
+end
