000b9bdc67110edefaf6b92a96de5a6f781e4ca4
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-20 12:28:26 +0200

    Added first cut on package install under cygwin using apt-cyg
    This makes it possible to use chef to install packages under cygwin.
    For this first version, apt-cyg must be pre-installed.

---
+++ cygwin/providers/package.rb
@@ -0,0 +1,8 @@
+﻿action :install do
+  execute "install #{new_resource.package_name}" do
+    command "apt-cyg install #{new_resource.package_name}"
+    action :nothing
+  end.run_action(:run)
+
+  new_resource.updated_by_last_action(true)
+end
---
+++ cygwin/resources/package.rb
@@ -0,0 +1,4 @@
+actions :install
+
+# name of the package to install
+attribute :package_name, :kind_of => String, :name_attribute => true
