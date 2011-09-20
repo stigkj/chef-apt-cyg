5f0a882bf476207fe87c13cf03ef78eef906ce05
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-20 12:30:27 +0200

    Filled in metadata about the cookbook

--- cygwin/metadata.rb
+++ cygwin/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures cygwin"
+maintainer       "Stig Kleppe-Jorgensen"
+maintainer_email "from.github@nisgits.net"
+license          "MIT"
+description      "Adds package manager that can be used under cygwin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
