450a7ec02c063ddbdcd7b8684acf3426cc0deb3f
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-21 11:44:43 +0200

    Added example of using If-Modified-Since for only downloading when a remote file is changed

--- cygwin/recipes/default.rb
+++ cygwin/recipes/default.rb
@@ -12,3 +12,22 @@ remote_file "/usr/local/bin/apt-cyg" do
   mode "0755"
   only_if { File.exists?("/usr/local/bin/apt-cyg") }
 end
+
+
+# FIXME check if this can be used against Google Code, that is, if it handles if-modified-since headers
+#remote_file "/tmp/couch.png" do
+#  source "http://couchdb.apache.org/img/sketch.png"
+#  action :nothing
+#end
+#
+#http_request "HEAD #{http://couchdb.apache.org/img/sketch.png}" do
+#  message ""
+#  url http://couchdb.apache.org/img/sketch.png
+#  action :head
+#
+#  if File.exists?("/tmp/couch.png")
+#    headers "If-Modified-Since" => File.mtime("/tmp/couch.png").httpdate
+#  end
+#
+#  notifies :create, resources(:remote_file => "/tmp/couch.png"), :immediately
+#end
