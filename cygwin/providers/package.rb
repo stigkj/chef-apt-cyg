db94479a7b36ac1fd35514f8b840a2e068bbd60c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-21 11:35:04 +0200

    Fixed reversed logic for checking if package should be installed
    Do not install if package is installed already. Do a grep on whole lines
    for the package names returned from 'apt-cyg show'.

--- cygwin/providers/package.rb
+++ cygwin/providers/package.rb
@@ -1,7 +1,8 @@
 ﻿action :install do
   execute "install #{new_resource.package_name}" do
     command "apt-cyg install #{new_resource.package_name}"
-    only_if "apt-cyg show | grep '^#{new_resource.package_name}$'"
+    # Do not install package if it is installed already
+    not_if "apt-cyg show | grep -x '#{new_resource.package_name}'"
     action :nothing
   end.run_action(:run)
 
