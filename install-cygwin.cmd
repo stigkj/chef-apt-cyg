651d5f8e110cd30aa9dc0cce0cda32ec363461eb
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-23 12:19:10 +0200

    First cut on a Windows batch file for setting up a Cygwin development environment
    It calls a ruby script from inside Cygwin that does the rest of the work

---
+++ install-cygwin.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+set HOME=c:\cygwin
+set SITE=ftp://ftp.sunet.se/pub/lang/cygwin
+
+echo Installing Cygwin under %HOME%...
+
+if not exist %HOME% goto default_packages
+
+echo "  default packages already installed"
+goto :extra_packages
+
+:default_packages
+
+echo "  default packages..."
+.\setup.exe -q -O -R %HOME% -s %SITE%
+
+
+:extra_packages
+
+if exist %HOME%\bin\make.exe goto extra_packages_exist
+
+echo "  extra packages needed (make, gcc-core, wget and ruby)..."
+.\setup.exe -q -O -R %HOME% -s %SITE% -P make,gcc-core,wget,ruby
+goto inside_cygwin
+
+:extra_packages_exist
+
+echo "  extra packages already installed"
+
+
+:inside_cygwin
+
+%HOME%\bin\bash.exe --login -c 'wget --no-check-certificate -q -O /tmp/iic.rb https://github.com/stigkj/chef-cygwin/install-inside-cygwin.rb; ruby /tmp/iic.rb'
+
+
+
+
