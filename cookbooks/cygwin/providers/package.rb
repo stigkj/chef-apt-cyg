831f6f78c0ead8f35e1c516c04854c7f009e2db8
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-23 18:54:24 +0200

    No need for specifying the action stuff
    Also have the not_if before the actual command as that reads better

--- cookbooks/cygwin/providers/package.rb
+++ cookbooks/cygwin/providers/package.rb
@@ -1,10 +1,9 @@
 action :install do
   execute "install #{new_resource.package_name}" do
-    command "apt-cyg install #{new_resource.package_name}"
     # Do not install package if it is installed already
     not_if "apt-cyg show | grep -x '#{new_resource.package_name}'"
-    action :nothing
-  end.run_action(:run)
+    command "apt-cyg install #{new_resource.package_name}"
+  end
 
   new_resource.updated_by_last_action(true)
 end
