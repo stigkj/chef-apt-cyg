9289c5f827125516e381198878ad45b8f8b3f579
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-23 15:13:44 +0200

    Fix the formatting of output
    No need to have do anything special for printing spaces; just add them before
    the text to be printed

--- install-cygwin.cmd
+++ install-cygwin.cmd
@@ -8,12 +8,12 @@ echo Installing Cygwin under %HOME%...
 
 if not exist %HOME% goto default_packages
 
-echo "  default packages already installed"
+echo   default packages already installed
 goto :extra_packages
 
 :default_packages
 
-echo "  default packages..."
+echo   default packages...
 .\setup.exe -q -O -R %HOME% -s %SITE%
 
 
@@ -21,18 +21,18 @@ echo "  default packages..."
 
 if exist %HOME%\bin\make.exe goto extra_packages_exist
 
-echo "  extra packages needed (make, gcc-core, wget and ruby)..."
+echo   extra packages needed (make, gcc-core, wget and ruby)...
 .\setup.exe -q -O -R %HOME% -s %SITE% -P make,gcc-core,wget,ruby
 goto inside_cygwin
 
 :extra_packages_exist
 
-echo "  extra packages already installed"
+echo   extra packages already installed
 
 
 :inside_cygwin
 
-echo "  running installation of chef from inside Cygwin"
+echo   running installation of chef from inside Cygwin
 %HOME%\bin\bash.exe --login -c "wget --no-check-certificate -q -O /tmp/iic.rb https://raw.github.com/stigkj/chef-cygwin/develop/install-inside-cygwin.rb; sed -i 's_#CURRENT_DIR#_%CURRENT_DIR%_'; ruby /tmp/iic.rb"
 
 
