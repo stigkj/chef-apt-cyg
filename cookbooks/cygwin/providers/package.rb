8d66dc06a0490addb73eb1d0167d43289a8ee978
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2011-09-23 19:09:22 +0200

    Ruby is not able to run apt-cyg directly
    Hopefully running the script through bash works better

--- cookbooks/cygwin/providers/package.rb
+++ cookbooks/cygwin/providers/package.rb
@@ -1,8 +1,8 @@
 action :install do
   execute "install #{new_resource.package_name}" do
     # Do not install package if it is installed already
-    not_if "apt-cyg show | grep -x '#{new_resource.package_name}'"
-    command "apt-cyg install #{new_resource.package_name}"
+    not_if "bash /usr/local/bin/apt-cyg show | grep -x '#{new_resource.package_name}'"
+    command "bash /usr/local/bin/apt-cyg install #{new_resource.package_name}"
   end
 
   new_resource.updated_by_last_action(true)
