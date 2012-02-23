02ba91b891a473d2fcaec65bcf9f50d4906e1a46
Stig Kleppe-Jørgensen <from.git@nisgits.net>
2012-02-23 16:54:46 +0100

    Update version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email  'from.chef-apt-cyg@nisgits.net'
 license           'MIT'
 description       'Installs apt-cyg package manager and use it as your package provider in cygwin'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.0.1'
+version           '1.0.1'
 recipe            'apt-cyg', 'Install apt-cyg'
 supports          'cygwin'
