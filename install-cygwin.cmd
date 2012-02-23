fe2a138eea504b2cebb258e4b3905078182967e7
Stig Kleppe-Jørgensen <from.git@nisgits.net>
2012-02-23 16:50:18 +0100

    Inline the ruby script for installing chef inside the batch script
    When inlining this script inside the Cygwin installation batch script, the installation dependency on github is removed. This is because the ruby script needed to be downloaded from github each time the installation was run.

--- install-cygwin.cmd
+++ install-cygwin.cmd
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+﻿setlocal EnableDelayedExpansion
 
 set CYGWIN=nodosfilewarning
 set CYGWIN_HOME_DEFAULT=c:\cygwin
@@ -43,7 +43,13 @@ echo   extra packages already installed
 :inside_cygwin
 
 echo   running installation of chef from inside Cygwin
-%CYGWIN_HOME%\bin\bash.exe --login -c "wget --no-check-certificate -q -O /tmp/iic.rb https://raw.github.com/stigkj/chef-apt-cyg/develop/install-inside-cygwin.rb; sed -i 's&#INSTALL_DIR#&%CURRENT_DIR%&' /tmp/iic.rb; ruby /tmp/iic.rb"
+
+@REM Everything from ___DATA___ (see below) and to the end of this file is a ruby script that is to be run
+@REM inside Cygwin. By way of some magic (see the next 2 lines) these lines are extracted into the file /tmp/iic.rb.
+﻿for /f "delims=:" %%a in ('findstr -n "^___" %0') do set "Line=%%a"
+(for /f "skip=%Line% tokens=* eol=_" %%a in ('type %0') do echo(%%a) > %CYGWIN_HOME%\tmp\iic.rb
+
+%CYGWIN_HOME%\bin\bash.exe --login -c "ruby /tmp/iic.rb"
 goto end
 
 :setup_not_found
@@ -52,4 +58,59 @@ echo setup.exe must be downloaded from http://cygwin.org and put in this directo
 echo.
 
 :end
+goto :EOF
 endlocal
+
+
+___DATA___
+#!/usr/bin/env ruby
+require 'fileutils'
+include FileUtils
+
+# TODO check exit codes from external processes (backticks)
+# Converts from DOS style to Cygwin style (C:\tmp --> /cygdrive/c/tmp)
+install_dir="!CURRENT_DIR!".downcase.gsub(":", "").insert(0, "/cygdrive/")
+
+puts
+puts 'Starting installation...'
+puts
+
+cd("/tmp") {
+    `gem --version 2>&1`
+
+    if ($?.exitstatus == 0)
+        puts '  RubyGems is already installed'
+    else
+        puts '  retrieving and unpacking RubyGems'
+
+        `wget -q -O - http://rubyforge.org/frs/download.php/75309/rubygems-1.8.10.tgz | tar zx`
+
+        cd("rubygems-1.8.10") {
+            puts '  installing RubyGems'
+            `ruby setup.rb --no-format-executable`
+        }
+    end
+
+    `chef-client --version 2>&1`
+
+    if ($?.exitstatus == 0)
+        puts '  Chef is already installed'
+    else
+        puts '  installing chef and related utilies'
+        `gem install ohai chef --no-rdoc --no-ri`
+    end
+}
+
+if (File.exist?("#{install_dir}/client.rb") && File.exist?("#{install_dir}/validation.pem"))
+    puts '  copying Chef client configuration files (client.rb & validation.pem) to /etc/chef'
+    mkdir_p("/etc/chef")
+    cp(["#{install_dir}/client.rb", "#{install_dir}/validation.pem"], "/etc/chef")
+    # Make sure the files are readable
+    chmod 0644, %w(/etc/chef/client.rb /etc/chef/validation.pem)
+
+    puts '  initial run of chef'
+    `chef-client -N cygwin`
+else
+    puts "  Chef client configuration files (client.rb & validation.pem) not found in #{install_dir}"
+    puts '  --> chef-client cannot be used before these files are present in /etc/chef'
+end
